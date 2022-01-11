422b7e33b51677e59a1ecc1ad34ae9f06aef8892
bekalshenoy <bekalshenoy@gmail.com>
2022-01-11 17:01:37 +0530

    bug fix

--- app.js
+++ app.js
@@ -260,6 +260,6 @@ app.get("/data", forwardAuthenticated, async (req, res) => {
   }
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
